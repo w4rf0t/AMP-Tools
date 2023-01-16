684613cf2c9cf7a1ad478159cb06c1eb55901c91
Truong An <79464700+anduu156@users.noreply.github.com>
2023-01-16 10:55:20 +0700

    Update install.sh

--- AutoRecon/install.sh
+++ AutoRecon/install.sh
@@ -4,4 +4,9 @@ cd ~
 git clone https://github.com/shmilylty/OneForAll.git; cd OneForAll; pip3 install -r requirements.txt; 
 git clone https://github.com/guelfoweb/knock.git; cd knock; pip3 install -r requirements.txt; 
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install github.com/tomnomnom/waybackurls@latest
+$ GO111MODULE=on go get -u -v github.com/lc/subjs@latest
+go get -u github.com/tomnomnom/gf
+echo 'source $GOPATH/src/github.com/tomnomnom/gf/gf-completion.bash' >> ~/.bashrc
+cp -r $GOPATH/src/github.com/tomnomnom/gf/examples ~/.gf
 pip3 install -r requirements.txt
