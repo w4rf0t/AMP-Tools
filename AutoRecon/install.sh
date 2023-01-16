cad9ea842dfbdd0ef902d2ab3c1a1143e3ad0d94
Truong An <79464700+anduu156@users.noreply.github.com>
2023-01-16 10:56:38 +0700

    Update install.sh

--- AutoRecon/install.sh
+++ AutoRecon/install.sh
@@ -6,6 +6,7 @@ git clone https://github.com/guelfoweb/knock.git; cd knock; pip3 install -r requ
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 go install github.com/tomnomnom/waybackurls@latest
 $ GO111MODULE=on go get -u -v github.com/lc/subjs@latest
+go install github.com/tomnomnom/qsreplace@latest
 go get -u github.com/tomnomnom/gf
 echo 'source $GOPATH/src/github.com/tomnomnom/gf/gf-completion.bash' >> ~/.bashrc
 cp -r $GOPATH/src/github.com/tomnomnom/gf/examples ~/.gf
