e49b359e2ad3ff23c25befae7fdea0a0419004a0
anduu156 <anduu150620@gmail.com>
2023-02-20 03:52:11 -0500

    fianl

--- AutoRecon/subdomain_recon.py
+++ AutoRecon/subdomain_recon.py
@@ -2,59 +2,69 @@ import subprocess
 import os
 import csv
 import json
-
-
-
-
-def call_oneforall(target):
-    
-    os.system(f"python3 /home/kali/OneForAll/oneforall.py --target {target} run")
-    with open(f'/home/kali/OneForAll/results/{target}.csv', 'r') as file:
-        reader = csv.reader(file)
-        row_num = 0
-        column = []
-        for row in reader:
-            if row_num > 0:
-                column.append(row[5])
-            row_num += 1
-        with open(f'AutoRecon/RESULT/{target}/subdomain_{target}.txt', 'a') as file:
-            file.write('\n'.join(column))   
-
-
+import requests
+import re
 def call_subfinder(target):
-    os.system(f"subfinder -d {target} -o AutoRecon/RESULT/{target}/subdomain_{target}.txt ")
+    print('Enumeration subdomain')
+    #Passive
+    # os.system(f"~/go/bin/subfinder -d {target} -silent -all -o Result/{target}/subdomain_{target}_subfinder.txt")
 
-# def call_knockpy(target):
-#     os.system(f"knockpy {target} -o AutoRecon/RESULT/{target}")
-#  #xu ly json
-#     with open(f"AutoRecon/RESULT/{target}/{target}_.json", "r") as f:
-#         data = json.load(f)
-# # Get a list of all the domain names that end in ".vietnamairport.vn"
-#     with open(f'AutoRecon/RESULT/{target}/subdomain_{target}.txt', 'a') as file:
-#         for domain in data:
-#             if domain.endswith(f".{target}"):
-#                 file.write(domain + "\n")
+    #bruteforce:
+    # os.system(f'~/go/bin/puredns bruteforce AutoRecon/Asset/dns-subdomain.txt {target} -l 5000 -r AutoRecon/Asset/resolvers.txt -w Result/{target}/subdomain_{target}_puredns.txt  > /dev/null')
+
+    os.system(f'cat Result/{target}/subdomain_{target}_*.txt | sort -u | uniq >> Result/{target}/subdomain_{target}.txt')
+    # os.system(f'rm Result/{target}/subdomain_{target}_*.txt')
+
+    proxies= {
+        'http': 'http://209.9.37.60:8087'
+    }
+    url = f"https://api.securitytrails.com/v1/domain/{target}/subdomains"
+    headers = {
+        "APIKEY": 'QH6pLgs9U2k5mCNpnTZc6FO5K4p1xWBB'
+    }
+    response = requests.get(url, headers=headers, proxies=proxies)
+    subdomains = set()
+    if response.status_code == 200:
+        response_json = response.json()
+        for subdomain in response_json["subdomains"]:
+            full_subdomain = f"{subdomain}.{target}"
+            subdomains.add(full_subdomain)
+    else:
+        pass
+
+    req = requests.get("https://crt.sh/?q=%.{d}&output=json".format(d=target))
+
+    if req.status_code == 200:
+        for subdomain in response_json["subdomains"]:
+            full_subdomain = f"{subdomain}.{target}"
+            subdomains.add(full_subdomain)
+    else:
+        pass
+    with open(f"Result/{target}/subdomain_{target}_securitytraials.txt", "a") as f:
+        for subdomain in subdomains:
+            f.write("%s\n" % subdomain)
 
 def sanitize_input(target):
-    os.system(f"awk '!seen[$0]++' AutoRecon/RESULT/{target}/subdomain_{target}.txt > AutoRecon/RESULT/{target}/final_subdomain_{target}.txt ")
-    os.system(f"cat AutoRecon/RESULT/{target}/final_subdomain_{target}.txt | httpx -sc -td -ip -o AutoRecon/RESULT/{target}/sub_status_{target}.txt")
-    with open(f'AutoRecon/RESULT/{target}/sub_status_{target}.txt',"r") as file1:
+    os.system(f"cat Result/{target}/subdomain_{target}_* >> Result/{target}/subdomain_{target}.txt; rm ")
+    os.system(f"awk '!seen[$0]++' Result/{target}/subdomain_{target}.txt > Result/{target}/final_subdomain_{target}.txt ")
+
+    os.system(f"cat Result/{target}/final_subdomain_{target}.txt | ~/go/bin/httpx -sc -td -ip -o Result/{target}/sub_status_{target}.txt")
+
+    with open(f'Result/{target}/sub_status_{target}.txt',"r") as file1:
         content = file1.readlines()
-        with open(f'AutoRecon/RESULT/{target}/sub_available_{target}.txt',"w") as file2:
-            with open(f'AutoRecon/RESULT/{target}/ip_available_{target}.txt',"w") as file3:
+        with open(f'Result/{target}/sub_available_{target}.txt',"w") as file2:
+            with open(f'Result/{target}/ip_available_{target}.txt',"w") as file3:
                 for line in content:
                     if "200" or "301" or "302" or "403" in line:
                         file2.write(line.split("[")[0]+"\n")
                         file3.write((line.split("[")[4]).replace("]", "")+"\n")
-    os.system(f"sed -i '/^[[:space:]]*$/d' AutoRecon/RESULT/{target}/sub_available_{target}.txt ; awk -i inplace '!seen[$0]++' AutoRecon/RESULT/{target}/ip_available_{target}.txt")
-                    
+    os.system(f"sed -i '/^[[:space:]]*$/d' Result/{target}/sub_available_{target}.txt ; awk -i inplace '!seen[$0]++' Result/{target}/ip_available_{target}.txt")
+    os.system(f'rm Result/{target}/sub_status_{target}.txt')
 def canlam2(target):
     try:
-        if not(os.path.exists(f'AutoRecon/RESULT/{target}')):
-            os.system(f"mkdir AutoRecon/RESULT/{target} | chmod 777 AutoRecon/RESULT/{target} ")
+        if not(os.path.exists(f'Result/{target}')):
+            os.system(f"mkdir Result/{target} | chmod 777 Result/{target} ")
     except Exception as e:
         pass
-    call_oneforall(target)
     call_subfinder(target)
-    call_knockpy(target)
     sanitize_input(target)
