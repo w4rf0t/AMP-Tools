29419f9ea07c4b11a47ab833fd157ecaec1acbf3
Truong An <79464700+anduu156@users.noreply.github.com>
2023-05-12 10:24:45 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,7 +16,10 @@ go install github.com/tomnomnom/qsreplace@latest
 go install github.com/tomnomnom/gf@latest
 go install github.com/0xsha/GoLinkFinder@latest
 git clone https://github.com/1ndianl33t/Gf-Patterns
+git clone https://github.com/tomnomnom/gf.git
+mv gf/examples/*.json ~/.gf
 mv Gf-Patterns/*.json ~/.gf
+rm -rf gf
 rm -rf Gf-Patterns
 pip3 install pytenable
 pip3 install python-Levenshtein
