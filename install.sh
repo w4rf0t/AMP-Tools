2abac1325e19ead76e056de4531908c6c244110b
Truong An <79464700+anduu156@users.noreply.github.com>
2023-07-18 16:15:42 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ mkdir .gf
 go install github.com/tomnomnom/qsreplace@latest
 go install github.com/tomnomnom/gf@latest
 go install github.com/0xsha/GoLinkFinder@latest
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 git clone https://github.com/1ndianl33t/Gf-Patterns
 git clone https://github.com/tomnomnom/gf.git
 mv gf/examples/*.json ~/.gf
